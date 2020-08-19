c084d112d9d002440f3c14fe0034155bf88b009d
Leon Darko <57622023+thah-leon@users.noreply.github.com>
2020-08-19 10:09:10 +0000

    CAF Architecture Launched

---
+++ js/myLoader.js
@@ -0,0 +1,11 @@
+
+  var myVar;
+
+  function myFunctionLoader() {
+      myVar = setTimeout(showPage, 2000);
+  }
+
+  function showPage() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("myDiv").style.display = "block";
+  }
